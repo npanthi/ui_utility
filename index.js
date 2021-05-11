e82979df901f52d56b35dd59f326b9c14cd91ddd
= <=>
2021-05-11 17:11:10 -0500

    added more utility functions

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 
 
 function isValidResponse(value){
-    console.log("Hey its working");
     return (value !== '' && value !== 'null' && value !== undefined && value !== null)
 }
+
+function isValidString(value){
+    return (isValidResponse(value) && typeof value === "string")
+}
+
+function isValidObj(value){
+    return (isValidResponse(value) && typeof value === "object")
+}
+
+function isValidObjWithLength(value){
+    return (isValidResponse(value) && typeof value === "object" && value.length > 0);
+}
+
+
 module.exports = {
-    isValidResponse:isValidResponse
+    isValidResponse:isValidResponse,
+    isValidString:isValidString,
+    isValidObject:isValidObj,
+    isValidObjectWithLength:isValidObjWithLength
 }
